ae92d58047b3ecb006b2e94d1459622e2002d598
Melih <86454129+melihshub@users.noreply.github.com>
2022-12-12 00:23:43 -0500

    added the solution for the first assignment

---
+++ Assignment1/challenge1.js
@@ -0,0 +1,19 @@
+/*Now that we know what variables are, 
+let's create one of our own. Below line 6, declare a variable named lastName 
+and assign it the value of your last name. 
+Make sure you wrap your name in quotation marks,
+e.g. "Smith". Here's another example in case:
+
+var myName = "Kyle";
+console.log(myName) // => "Kyle"
+
+Now, go back to line 1 and modify the value of firstName to be your first name. 
+Uncomment the console.log statement on line 10 and run your code; 
+you should see Jenny replaced by your 
+first name—unless your first name is Jenny,of course.*/
+
+var firstName = "Melih";
+var lastName = "Kartal";
+
+console.log(firstName);
+console.log(lastName);
