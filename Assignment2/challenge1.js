10fdbf90a3630c4835cdd5f930b17d820812e8bd
Melih Kartal <7kartalm7@gmail.com>
2022-12-17 21:06:48 -0500

    Added 2 more challenges

---
+++ Assignment2/challenge1.js
@@ -0,0 +1,10 @@
+// Challenge
+
+// Create a variable called fourthItem and assign it the value of the fourth item in the horror array ('Ghostface'). 
+//Then console.log fourthItem to see the output.
+
+const horror = ['Freddy', 'Jason', 'Michael', 'Ghostface', 'Chucky'];
+
+let fourthItem = horror[3];
+
+console.log(fourthItem);
---
+++ Assignment2/challenge2.js
@@ -0,0 +1,7 @@
+// Challenge
+
+// Use a built-in JavaScript method to add another show to the netflixShows array. Then log your updated array to the console.
+
+const  netflixShows = ["Orange is the New Black", "Black Mirror", "Chewing Gum"];
+
+netflixShows.push("Peaky Blinders");
